ebbdb380f58c829cf5b4c80c5aed21777ac20bf9
ydnactnim <ydnactnim@gmail.com>
2024-10-25 14:55:27 +0900

    Practice Clear
    Practice Clear

--- typing_game/script.js
+++ typing_game/script.js
@@ -16,18 +16,43 @@ const quoteElement = document.getElementById("quote");
 const messageElement = document.getElementById("message");
 const typedValueElement = document.getElementById("typed-value");
 
+document.getElementById("typed-value").disabled = true;
+
+document.getElementById("icon").addEventListener("click", function () {
+  document.body.classList.toggle("black-mode");
+  document.body.classList.toggle("white-mode");
+
+  const svgElement = document.getElementById("icon");
+  svgElement.classList.toggle("invert-colors");
+
+  if (document.body.classList.contains("black-mode")) {
+    quoteElement.childNodes[wordIndex].className = "highlight_dark";
+  } else {
+    quoteElement.childNodes[wordIndex].className = "highlight_white";
+  }
+});
+
 document.getElementById("start").addEventListener("click", () => {
-  const quoteIndex = Math.floor(Math.random() * quotes.length); // 무작위 인덱스
-  생성;
+  const quoteIndex = Math.floor(Math.random() * quotes.length); // 무작위 인덱스 생성;
   const quote = quotes[quoteIndex]; // 무작위 인덱스 값으로 인용문 선택
+
   words = quote.split(" "); // 공백 문자를 기준으로 words 배열에 저장
   wordIndex = 0; // 초기화
+
   const spanWords = words.map(function (word) {
     return `<span>${word} </span>`;
   });
+
+  // input 활성화
+  document.getElementById("typed-value").disabled = false;
+
   // span 태그로 감싼 후 배열에 저장
   quoteElement.innerHTML = spanWords.join(""); // 하나의 문자열로 결합 및 설정
-  quoteElement.childNodes[0].className = "highlight"; // 첫번째 단어 강조
+  quoteElement.childNodes[0].className = document.body.classList.contains(
+    "black-mode"
+  )
+    ? "highlight_dark"
+    : "highlight_white"; // 첫번째 단어 강조
   messageElement.innerText = ""; // 메시지 요소 초기화
   typedValueElement.value = ""; //입력 필드 초기화
   typedValueElement.focus(); // 포커스 설정
@@ -37,6 +62,7 @@ document.getElementById("start").addEventListener("click", () => {
 typedValueElement.addEventListener("input", () => {
   const currentWord = words[wordIndex]; // 현재 타이핑할 단어를 currentWord 에 저장
   const typedValue = typedValueElement.value; // 입력한 값을 typedValue에 저장
+
   if (typedValue === currentWord && wordIndex === words.length - 1) {
     // 마지막 단어까지 정확히 입력했는 지 체크
     const elapsedTime = new Date().getTime() - startTime; // 타이핑에 소요된 시간 계산
@@ -44,6 +70,13 @@ typedValueElement.addEventListener("input", () => {
       elapsedTime / 1000
     } seconds.`; // 타이핑 완료 메시지
     messageElement.innerText = message; //생성된 메시지 화면에 표시
+
+    // input 비활성화
+    document.getElementById("typed-value").disabled = true;
+
+    // 이벤트 리스너 비활성화
+    typedValueElement.removeEventListener("click", () => {});
+    typedValueElement.removeEventListener("input", () => {});
   } else if (typedValue.endsWith(" ") && typedValue.trim() === currentWord) {
     // 입력된 값이 공백으로 끝났는지와 공백을 제거한 값이 현재 단어와 일치하는 지 확인
     typedValueElement.value = ""; // 입력 필드 초기화하여 다음 단어 입력 준비
@@ -52,7 +85,10 @@ typedValueElement.addEventListener("input", () => {
       // 모든 강조 표시 제거
       wordElement.className = ""; // 클래스 제거
     }
-    quoteElement.childNodes[wordIndex].className = "highlight"; // 다음으로 타이핑할 단어에 클래스 추가
+    quoteElement.childNodes[wordIndex].className =
+      document.body.classList.contains("black-mode")
+        ? "highlight_dark"
+        : "highlight_white"; // 다음으로 타이핑할 단어에 클래스 추가
   } else if (currentWord.startsWith(typedValue)) {
     //현재 단어의 일부를 맞게 입력하고 있는 지 확인
     typedValueElement.className = ""; // 올바르면 클래스 제거
